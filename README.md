cb8496588c6bc0f31ecc2d43072bc52b6b986e60
Ludovico Rossi <lurossi@c2r4s2.42roma.it>
2026-09-08 18:03:03 +0200

    new strategy and implementation

--- README.md
+++ README.md
@@ -5,17 +5,12 @@
 WORK IN PROGRESS
 
 
-registrare numero di percorsi massimo pari al numero di droni
-ogni percorso tiene al massimo la capacita' piu piccola sia di celle che di connessioni
-funzione che calcola quanti turni da un punto a un altro seguendo un percorso memorizzato
-usare percorso piu breve finche la ripetizione non supera la differenza con il pu breve successivo
-valutare quanto si overlappano i percorsi, e se si arriva in posizioni sensibili in contemporanea o meno
-movimento per zona restricted solo se la successiva puo essere liberata in tempo
-coefficiente di priorita, scegliere a pari mosse quello con priorita piu altra
+numero di mosse n
+trovare percorso piu veloce e assegnarlo a 1 drone, rimuovere 1 spazio dalle caselle visitate.
+salvare percorso in un array con numero di mosse annesso
+ripetere finche non finiscono i droni o il numero di mosse n necessario non aumenta
 
-algoritmo
-dijkstra per il piu efficiente, dividere per la capienza per avere un valore effettivo per drone
-opzioni: 
-capienza resta uguale o diminuisce nel tempo: solo dijkstra e altri percorsi indipendenti
+se n aumenta i nodi hanno di nuovo tutto lo spazio che serve
+riutilizzare tutti i percorsi salvati facendo aspettare per n - n.percorso turni
 
-capienza aumenta dopo un po, sneza diminuzione successiva: dijkstra o yen da dove aumentano verso l'inizio, senza usare il piu efficiente originale e fino a massima capienza del ottimizzato. se poi l'ottimizzato aumenta ulteriormente, nuovo dijkstra o yen e limitare la capienza dei nodi gia usati nel secondo percorso o bloccarli. 
+se non si trovano altri percorsi, ripetere percorsi salvati finche non finiscono i droni
---
+++ path.py
@@ -0,0 +1,62 @@
+from map_creator import Node, ZoneType
+import math
+
+def faster_path(starting_node: Node):
+    if starting_node.name is not "start_hub":
+        raise ReferenceError("start_hub not provided correctly")
+    fail = False
+    visited = []
+    unvisited = [node for node in starting_node.nodes]
+    unvisited.remove("start_hub")
+    nodes_map = {node:[math.inf, None, False] for node in starting_node.nodes}
+    nodes_map["start_hub"] = [0, None, False]
+    chosen_node = starting_node
+    while chosen_node.name != "goal":
+        next_node = None
+        visited.append(chosen_node.name)
+        if chosen_node.name in unvisited:
+            unvisited.remove(chosen_node.name)
+        cost_up_to_here = nodes_map[chosen_node.name][0]    
+        for contact in chosen_node.contacts:
+            contact_node, max_through = chosen_node.contacts[contact]
+            if max_through == 0 or contact_node.name in visited:
+                continue
+            if contact_node.cap == 0:
+                if contact_node.name in unvisited:
+                    unvisited.remove(contact_node.name)
+                visited.append(contact_node.name)
+                nodes_map[contact_node.name] = [-1, None, False]
+                continue
+            if contact_node.zone == ZoneType.PRIORITY:
+                nodes_map[contact_node.name][2] = True
+            if (contact_node.zone in (ZoneType.PRIORITY, ZoneType.NORMAL)) and cost_up_to_here + 1 < nodes_map[contact_node.name][0]:
+                nodes_map[contact_node.name][0] = cost_up_to_here + 1
+                nodes_map[contact_node.name][1] = chosen_node
+            elif contact_node.zone == ZoneType.RESTRICTED and cost_up_to_here + 2 < nodes_map[contact_node.name][0]:
+                nodes_map[contact_node.name][0] = cost_up_to_here + 2
+                nodes_map[contact_node.name][1] = chosen_node
+        for nodes in unvisited:
+            if next_node is None:
+                if nodes_map[nodes.name][0] != math.inf: 
+                    next_node = nodes
+                continue
+            elif nodes_map[nodes.name][0] < nodes_map[next_node.name][0] or (
+                (nodes_map[nodes.name][0] == nodes_map[next_node.name][0]) and nodes_map[nodes.name][2]):
+             next_node = nodes
+        
+        if next_node is not None:
+            chosen_node = next_node
+        else:
+            fail = True
+            break
+    if fail:
+        return None
+    path = ["goal"]
+    prev_node = nodes_map["goal"][1]
+    while prev_node.name != "start_hub":
+        path.append(prev_node.name)
+        if prev_node.zone == ZoneType.RESTRICTED:
+            path.append("travelling to" + prev_node.name)
+        prev_node = nodes_map[prev_node.name][1]
+    path.reverse()
+    return (len(path), path)
